ce8410a32a8bbc7cd46e97f2cd7f5276fb6b472b
howardcheung <howard.at@gmail.com>
2015-01-15 19:39:24 -0700

    Add function to make parity plot.

--- Code/misc_func.py
+++ Code/misc_func.py
@@ -6,6 +6,7 @@
 """
 
 import numpy as np
+import pylab as plt
 
 
 class OperatingPoint:
@@ -206,15 +207,99 @@ def K2R(T_K):
     """
         Convert temperature in Kelvin to Rankine , code from ACHP
 
-        Returns:
+        Parameters:
         ===========
         T_K: float
             temperature in Kelvin
 
-        Parameters:
+        Returns:
         ===========
         T_R: float
             temperature in degrees Rankine
 
     """
     return T_K*9./5.
+
+
+def parity_plot(
+        x_data, uncer_x_data, x_label,
+        y_data, uncer_y_data, y_label,
+        filepath
+        ):
+    """
+        This function prints a parity plot with deviation
+        lines and save it as pdf
+
+        Parameters:
+        ===========
+        x_data: list or numpy array
+            data to be plotted on x-axis
+
+        uncer_x_data: list or numpy array
+            error bar on x-axis
+
+        x_label: string
+            label text on x-axis
+
+        y_data: list or numpy array
+            data to be plotted on y-axis
+
+        uncer_y_data: list or numpy array
+            error bar on y-axis
+
+        y_label: string
+            label text on y-axis
+
+        path: string
+            path and filename where the figure is saved
+
+    """
+
+    fig = plt.figure(1)
+    plt.scatter(
+        x_data, y_data, s=30, c='b', marker='o'
+    )
+    plt.legend(loc=2)
+    plt.errorbar(
+        x_data, y_data, xerr=uncer_x_data,
+        yerr=uncer_y_data, fmt='bo'
+    )
+    plt.xlabel(
+        x_label, fontsize='x-large'
+    )
+    plt.ylabel(
+        y_label,
+        fontsize='x-large', multialignment='center'
+    )
+    plt.rc('xtick', labelsize='x-large')
+    plt.rc('ytick', labelsize='x-large')
+    graph_filename = filepath
+    plt.gcf().subplots_adjust(bottom=0.15)
+    plt.gcf().subplots_adjust(left=0.15)
+    datamax = max([
+        max(x_data), max(y_data),
+    ])*1.1
+    datamin = min([
+        min(x_data), min(y_data),
+    ])*0.9
+    degreeline = [datamin, datamax]
+    plt.plot(degreeline, degreeline, 'r--')
+    a = max(abs(np.array(y_data)-np.array(x_data)))
+    tenpercentline = [datamin-a, datamax-a]
+    plt.plot(degreeline, tenpercentline, 'b:')
+    s = '- %2.2f' % a
+    plt.figtext(
+        0.55, 0.45, s+' W',
+        fontdict=None, size='x-large', color='k',
+        horizontalalignment='left'
+    )
+    tenpercentline = [datamin+a, datamax+a]
+    plt.plot(degreeline, tenpercentline, 'b:')
+    s = '+ %2.2f' % a
+    plt.figtext(
+        0.5, 0.65, s+' W',
+        fontdict=None, size='x-large', color='k',
+        horizontalalignment='right')
+    plt.axis([datamin, datamax, datamin, datamax])
+    plt.savefig(graph_filename, dpi=300)
+    plt.close(fig)
